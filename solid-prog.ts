b3f3b60b40a3920f63f0bf2e582a054de61d78a7
Abdul Salam <abdulsalam121196@gmail.com>
2021-01-22 16:25:16 +0700

    reinit regex custom

--- solid-prog.ts
+++ solid-prog.ts
@@ -26,7 +26,7 @@ class EmailValidation implements iValidate {
     public regexPattern: string;
 
     constructor() {
-        this.regexPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        this.regexPattern = `/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`;
     }
 
     validate(data: iUser): boolean {
